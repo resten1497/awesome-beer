869c2f68b716fee6f9cc7f264aef56064f1d5ab6
Resten1497 <saup1965@gmail.com>
2022-10-02 01:58:44 +0900

    UPDATE : Split Usage ADD / Data Update

---
+++ src/split-csv.js
@@ -0,0 +1,28 @@
+const Papa = require("papaparse");
+const fs = require("fs");
+const dataStream = fs.readFileSync("beer.csv", { encoding: "utf-8" });
+const fsExtra = require("fs-extra");
+try {
+  const results = Papa.parse(dataStream, { header: true });
+
+  fsExtra.emptyDirSync("./data/");
+  console.log("✂️ 데이터 분할중 ... ");
+
+  const breweries = results.data;
+  for (let brewery of breweries) {
+    let { sidoNm } = brewery;
+    if (sidoNm === undefined) continue;
+    let sidoNmPath = `./data/${sidoNm}.md`;
+    let data = `#### [${brewery.name}](${brewery.naverUrl}) \n`;
+    data += ` - 주소 : ${brewery.address}\n`;
+    data += `- 종류 : ${brewery.beerType}\n`;
+    data += ` - 특징 : ${brewery.desc}\n`;
+    data += `- [홈페이지](${brewery.homepageUrl})\n`;
+    fs.appendFileSync(sidoNmPath, data);
+  }
+
+  console.log("👍 데이터 분할 완료!");
+  console.log("총 " + breweries.length + " 개의 데이터를 분할했습니다.");
+} catch (e) {
+  console.log("❌ 데이터 분할 실패!");
+}
